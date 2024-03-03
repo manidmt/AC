6f0a68c3a802c9747b4db9b7c319f712d9242677
manidmt <manidmt5@gmail.com>
2024-03-03 17:18:05 +0100

    Uploading all docs

---
+++ bp0/HelloOMP2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main(void) {
+
+#pragma omp parallel
+    printf("(%d:Hello)",
+    omp_get_thread_num());
+    printf("(%d: World)",
+    omp_get_thread_num());
+    printf("\n");
+return(0);
+}
